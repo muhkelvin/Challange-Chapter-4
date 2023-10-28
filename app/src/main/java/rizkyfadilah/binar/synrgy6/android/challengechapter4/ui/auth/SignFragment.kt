03a59df8014b32c293fe9be25091bc22f65f9587
muhammadkelvin <muhkelvin36@gmail.com>
2023-10-28 09:01:55 -0600

    implementt

--- app/src/main/java/rizkyfadilah/binar/synrgy6/android/challengechapter4/ui/auth/SignFragment.kt
+++ app/src/main/java/rizkyfadilah/binar/synrgy6/android/challengechapter4/ui/auth/SignFragment.kt
@@ -1,60 +1,62 @@
 package rizkyfadilah.binar.synrgy6.android.challengechapter4.ui.auth
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import rizkyfadilah.binar.synrgy6.android.challengechapter4.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import rizkyfadilah.binar.synrgy6.android.challengechapter4.MainActivity
+import rizkyfadilah.binar.synrgy6.android.challengechapter4.databinding.FragmentSignBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSignBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign, container, false)
+    ): View {
+        binding = FragmentSignBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvBelum.setOnClickListener {
+            SignFragmentDirections.actionSignFragmentToSignUpFragment().also {
+                findNavController().navigate(it)
             }
+        }
+
+        sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        binding.btnLogin.setOnClickListener {
+            val username = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (checkLogin(username, password)) {
+                startActivity(Intent(requireContext(), MainActivity::class.java))
+                requireActivity().finish()
+            } else {
+                // Handle login failure
+                // For simplicity, showing a toast message
+                // You may want to implement a proper UI feedback
+                Toast.makeText(requireContext(), "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkLogin(username: String, password: String): Boolean {
+        val storedUsername = sharedPreferences.getString("username", "") ?: ""
+        val storedPassword = sharedPreferences.getString("password", "") ?: ""
+
+        return username == storedUsername && password == storedPassword
     }
 }
